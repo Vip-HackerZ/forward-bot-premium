80f116800aec5d76dfd1efc0dfd0db4588cf21f0
Vip-HackerZ <ommore2021107@gmail.com>
2024-11-03 17:41:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = os.environ.get("API_ID", "18156248")
+    API_HASH = os.environ.get("API_HASH", "db946fb6805b1a698c679626b617e77a")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7504169790:AAGzS8YTC9BCTfIRRtEU-ray7pEqndrlw5s") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot-premium") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "Stubborn")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://uploaderbot:uploaderbot@cluster0.koh1t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "uploaderbot")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '2032347579').split()]
 
 
 class temp(object): 
